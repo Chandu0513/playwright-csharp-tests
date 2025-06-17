2414d238a5a7522523f1e51a7b11db42e7062055
Chandu0513 <bakulapallychandra@gmail.com>
2025-06-17 18:44:51 +0530

    reverte

--- deploy.ps1
+++ deploy.ps1
@@ -13,4 +13,4 @@ if (-Not (Test-Path $DeployPath)) {
 Write-Host "Copying files..."
 Copy-Item "$BuildOutput\*" "$DeployPath" -Recurse -Force
 
-Write-Host "Hey explorer from optimworks. The Deployment completed successfully."
+Write-Host "Hey explorer from optimworks. The Deployment completed successfully.Check"
