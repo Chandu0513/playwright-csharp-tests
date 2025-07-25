f017436e894f71ebb74cb78ad41626f682ca4bae
Chandu0513 <bakulapallychandra@gmail.com>
2025-07-25 16:08:35 +0530

    DemoForTestwith schedulr

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,20 @@
 # deploy.ps1
 
 $BuildOutput = "C:\actions-runner\_work\playwright-csharp-tests\playwright-csharp-tests\bin\Release\net8.0"
-$DeployPath = "C:\AfterCIRunner\MyProject"
+$DeployPath = "C:\AfterCIRunner\MyProjectFiles"
 
 Write-Host "Starting Deployment..."
 
-if (-Not (Test-Path $DeployPath)) {
+# Clean the target folder if it exists
+if (Test-Path $DeployPath) {
+    Write-Host "Clearing existing deployment folder..."
+    Remove-Item -Path "$DeployPath\*" -Recurse -Force
+} else {
     Write-Host "Creating deployment folder..."
     New-Item -Path $DeployPath -ItemType Directory -Force
 }
 
-Write-Host "Copying files..."
+Write-Host "Copying new build output..."
 Copy-Item "$BuildOutput\*" "$DeployPath" -Recurse -Force
 
-Write-Host "Hey explorer from optimworks. The Deployment completed successfully. You can check the report "
+Write-Host "Hey explorer from optimworks. The deployment completed successfully. Check the latest report in the deployment path."
